sicelo/iio-sensor-proxy

commit ac053f8a120866bea53e8dd8c459a57a5053a5af

iio: Remove fscanf usage in _write_sysfs_string()
From Coverity:
iio-sensor-proxy-3.3/src/iio-buffer-utils.c:435: dont_call: "fscanf" assumes an arbitrarily long string, so callers must use correct precision specifiers or never use "fscanf".

This also fixes the temp variable being reused to parse the contents of
the sysfs file before being freed. How this behaves depends on the
length of the sysfs file path and the length of the string inside the
file.

src/iio-buffer-utils.c
@@ -402,6 +402,7 @@ _write_sysfs_string (const char *filename,
 {
 	g_autoptr(FILE) sysfsfp = NULL;
 	g_autofree char *temp = NULL;
+	g_autofree char *contents = NULL;
 
 	temp = g_build_filename (basedir, filename, NULL);
 	sysfsfp = fopen (temp, "w");
@@ -409,19 +410,16 @@ _write_sysfs_string (const char *filename,
 		return -errno;
 	}
 	fprintf(sysfsfp, "%s", val);
-	g_clear_pointer (&sysfsfp, fclose);
 
 	/* Verify? */
 	if (!verify)
 		return 0;
-	sysfsfp = fopen(temp, "r");
-	if (sysfsfp == NULL) {
-		return -errno;
-	}
-	if (fscanf(sysfsfp, "%s", temp) != 1 ||
-	    strcmp(temp, val) != 0) {
-		g_warning ("Possible failure in string write of %s Should be %s written to %s\\%s\n",
-			   temp, val, basedir, filename);
+	g_clear_pointer (&sysfsfp, fclose);
+	if (!g_file_get_contents (temp, &contents, NULL, NULL) ||
+	    g_strcmp0 (contents, val) != 0) {
+		g_message ("WTA");
+		g_warning ("Possible failure in string write of '%s' Should be '%s' written to %s\n",
+			   contents ?: "(null)", val, temp);
 		return -1;
 	}
 

commit ad2c296b46361df16b6362b8af8a5748e051c024

iio: Fix sysfs attribute verification failing
sysfs usually appends newlines at the end of attributes to make them
easier to consume, but the test suite can't test for that.

** (iio-sensor-proxy:4882): WARNING **: 14:14:46.792: Possible failure in string write of 'accel_3d-dev0
' Should be 'accel_3d-dev0' written to /sys/devices/platform/AMDI0010:02/i2c-2/i2c-BMA250E:00/0018:6243:0001.0002/HID-SENSOR-200073.2.auto/iio:device0/trigger/current_trigger

Closes: #347

Fixes: ac053f8a120866bea53e8dd8c459a57a5053a5af

src/iio-buffer-utils.c
@@ -416,7 +416,7 @@ _write_sysfs_string (const char *filename,
 		return 0;
 	g_clear_pointer (&sysfsfp, fclose);
 	if (!g_file_get_contents (temp, &contents, NULL, NULL) ||
-	    g_strcmp0 (contents, val) != 0) {
+	    g_strcmp0 (g_strchomp (contents), val) != 0) {
 		g_warning ("Possible failure in string write of '%s' Should be '%s' written to %s\n",
 			   contents ?: "(null)", val, temp);
 		return -1;